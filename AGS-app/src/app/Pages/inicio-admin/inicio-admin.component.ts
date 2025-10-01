97f33c7da84e59be6a77195f3c649087c5548e1a
tbalma <balmaceda0205@gmail.com>
2025-10-01 17:47:40 -0300

    Agrego pagina de login con funcionalidad de iniciar sesion

---
+++ AGS-app/src/app/Pages/inicio-admin/inicio-admin.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-inicio-admin',
+  standalone: false,
+  templateUrl: './inicio-admin.component.html',
+  styleUrl: './inicio-admin.component.css'
+})
+export class InicioAdminComponent {
+
+}
--- AGS-app/src/app/Services/user.service.ts
+++ AGS-app/src/app/Services/user.service.ts
@@ -1,9 +1,32 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 
 @Injectable({
   providedIn: 'root'
 })
 export class UserService {
+  url = "https://localhost:7198/AGS/users"
+  private userId:any
 
-  constructor() { }
+  constructor(private httpClient: HttpClient) { }
+
+  CreateUser(obj:any){
+    return this.httpClient.post(this.url + "/CreateUser", obj)
+  }
+
+  Login(obj:any){
+    return this.httpClient.post(this.url + "/Login", obj)
+  }
+
+  GetUsers(){
+    return this.httpClient.get(this.url + "/GetUsers")
+  }
+
+  getUserId(){
+    return this.userId
+  }
+
+  setUserId(id:any){
+    this.userId = id
+  }
 }
