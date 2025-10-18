9aae5a2afcf72bd0edb7eccc3523c58e6f8cb92b
tbalma <balmaceda0205@gmail.com>
2025-10-18 12:48:57 -0300

    Agrego el footer

--- AGS-app/src/app/Components/footer/footer.component.spec.ts
+++ AGS-app/src/app/Components/footer/footer.component.spec.ts
@@ -1,18 +1,18 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { LockedComponent } from './locked.component';
+import { FooterComponent } from './footer.component';
 
-describe('LockedComponent', () => {
-  let component: LockedComponent;
-  let fixture: ComponentFixture<LockedComponent>;
+describe('FooterComponent', () => {
+  let component: FooterComponent;
+  let fixture: ComponentFixture<FooterComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [LockedComponent]
+      declarations: [FooterComponent]
     })
     .compileComponents();
 
-    fixture = TestBed.createComponent(LockedComponent);
+    fixture = TestBed.createComponent(FooterComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
