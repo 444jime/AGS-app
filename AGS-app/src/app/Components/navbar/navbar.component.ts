85b2325eb24218438c0d477e71ac54e86e21d6a6
tbalma <balmaceda0205@gmail.com>
2025-10-18 13:01:24 -0300

    Agrego funcionalidad a la navbar
    Aparece o desaparece botón "admin" según si el usuario ha iniciado sesión o no.

--- AGS-app/src/app/Components/navbar/navbar.component.ts
+++ AGS-app/src/app/Components/navbar/navbar.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-navbar',
@@ -6,6 +7,26 @@ import { Component } from '@angular/core';
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.css'
 })
-export class NavbarComponent {
+export class NavbarComponent implements OnInit {
+  usuarioRegistrado = false
 
+  constructor(private router: Router) {}
+
+  ngOnInit(): void {
+    console.log("dd"+ this.usuarioRegistrado)
+    let user_state = localStorage.getItem("user_state")
+
+    if (user_state === "true"){
+        this.usuarioRegistrado = true
+        this.router.navigate([this.router.url]);
+        console.log(user_state)
+    }
+  }
+
+  LogOut(){
+    localStorage.removeItem("user_state")
+    localStorage.removeItem("userId");
+    this.usuarioRegistrado = false;
+    this.router.navigate(['AGS/inicio'])
+  }
 }
