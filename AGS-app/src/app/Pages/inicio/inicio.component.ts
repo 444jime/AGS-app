e0d65a631171da684274bbd24b6b1ab42fd6f15e
tbalma <balmaceda0205@gmail.com>
2025-11-20 17:27:46 -0300

    Agrego imagen de logo y header

--- AGS-app/src/app/Pages/inicio/inicio.component.ts
+++ AGS-app/src/app/Pages/inicio/inicio.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { ProyectosService } from '../../Services/proyectos.service';
 
 @Component({
@@ -7,7 +7,27 @@ import { ProyectosService } from '../../Services/proyectos.service';
   templateUrl: './inicio.component.html',
   styleUrl: './inicio.component.css'
 })
-export class InicioComponent {
+export class InicioComponent implements OnInit{
   
+  images:any;
+  imagenHeader:any;
+  constructor(private proyectosService:ProyectosService) {}
+
+  ngOnInit(): void {
+    this.getImages()
+  }
+
+  getImages(){
+    this.proyectosService.getImages().subscribe({
+      next: (res) => {
+        this.images = res
+        const header = this.images.find((img: any) => img.nombre === "Aeropuerto SMA 2")
+        this.imagenHeader = header ? header.url : ''
+        console.log(res)
+        console.log(this.imagenHeader)
+      },
+      error: (err) => console.error(err)
+    })
+  }
   
 }
