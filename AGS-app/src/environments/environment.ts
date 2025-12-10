83a90d54614c3e9e5552263bee0b10eef345dc40
tbalma <balmaceda0205@gmail.com>
2025-12-10 19:33:12 -0300

    fix: Arreglo errores de environment

--- AGS-app/src/environments/environment.ts
+++ AGS-app/src/environments/environment.ts
@@ -1,5 +1,6 @@
 export const environment = {
   production: true,
   // apiUrl: 'https://localhost:7198/AGS'
-  apiUrl: 'https://ags-api-u74x.onrender.com/AGS'
+  // apiUrl: 'https://ags-api-u74x.onrender.com/AGS'
+  apiUrl: 'https://ags-web-production.up.railway.app/AGS'
 };
